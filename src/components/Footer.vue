b7de0305daa67205cad7c1c80bdb95ee0447f39f
NotPeppa <astraycat@qq.com>
2025-04-15 02:22:33 +0800

    修改页脚

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,8 +16,8 @@
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
           &amp;&nbsp;Made&nbsp;by
-          <a :href="config.github" target="_blank">
-            {{ config.author }}
+          <a :href="siteUrl" target="_blank">
+            {{ siteAuthor }}
           </a>
         </span>
         <!-- 站点备案 -->
